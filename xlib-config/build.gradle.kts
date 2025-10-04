8467fbc535ce3cbf5a69cd165edf9aa880196973
Alexander <alexanderthegoood@gmail.com>
2025-10-04 19:33:36 +0300

    add license warn

--- xlib-config/build.gradle.kts
+++ xlib-config/build.gradle.kts
@@ -1,3 +1,8 @@
+/*
+ * Copyright 2025 alexthegoood. Use of this source code is governed by the Apache 2.0 license
+ * that can be found in the LICENSE file.
+ */
+
 plugins {
     convention.project
     convention.publication
