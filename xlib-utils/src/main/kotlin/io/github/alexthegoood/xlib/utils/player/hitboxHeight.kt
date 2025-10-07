9b7532559276d93744b54f6b24859140de8d05bb
Alexander <alexanderthegoood@gmail.com>
2025-10-07 14:06:01 +0300

    add KDoc & rename+relocate function

--- xlib-utils/src/main/kotlin/io/github/alexthegoood/xlib/utils/player/hitboxHeight.kt
+++ xlib-utils/src/main/kotlin/io/github/alexthegoood/xlib/utils/player/hitboxHeight.kt
@@ -3,12 +3,17 @@
  * that can be found in the LICENSE file.
  */
 
-package io.github.alexthegoood.xlib.utils
+package io.github.alexthegoood.xlib.utils.player
 
 import org.bukkit.attribute.Attribute
 import org.bukkit.entity.Player
 
-fun Player.getHitboxHeight() : Float { player
+/**
+ * Returns the highest point of the player based on his [Attribute.SCALE].
+ *
+ * @return The highest point of the player hitbox.
+ */
+fun Player.getHitboxHeight() : Float {
     val playerHitboxHeight: Float = 1.7671875f
     val playerScaleAttribute: Float = player?.getAttribute(Attribute.SCALE)?.value?.toFloat() ?: 1.0f
     return playerHitboxHeight*playerScaleAttribute
